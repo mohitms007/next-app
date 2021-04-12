f2439447470a9bcca0cddcfade43c5a62a5b4f3a
DragonMaster <ms235389@gmail.com>
2021-04-12 10:06:10 +0530

    Intialisation of file_structure

---
+++ pages/eom.tsx
@@ -0,0 +1,11 @@
+export const EOM = () => {
+
+     return (
+          <div className="page-container">
+               Employeee Of the Month
+          </div>
+     )
+
+}
+
+export default EOM;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div>
+        Hello World
+    </div>
+  )
+}
+
+
+// b104397bfd4d467fab25dc446b374830
